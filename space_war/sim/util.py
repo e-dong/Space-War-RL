60b47b92c21fb2e36a39fc932d99e7b8c2273b95
Eric Dong <eric2043@gmail.com>
2024-09-06 22:52:54 -0400

    Fixed pyright detection

--- space_war/sim/util.py
+++ space_war/sim/util.py
@@ -10,15 +10,16 @@ def check_overlapping_sprites(
 ):
     """Returns the horizontal and vertical overlap between two sprites."""
     overlap_x, overlap_y = (0, 0)
-    if sprite.rect.centerx < sprite_other.rect.centerx:
-        overlap_x = sprite.rect.right - sprite_other.rect.left
-    elif sprite.rect.centerx > sprite_other.rect.centerx:
-        overlap_x = -(sprite_other.rect.right - sprite.rect.left)
+    if sprite.rect and sprite_other.rect:
+        if sprite.rect.centerx < sprite_other.rect.centerx:
+            overlap_x = sprite.rect.right - sprite_other.rect.left
+        elif sprite.rect.centerx > sprite_other.rect.centerx:
+            overlap_x = -(sprite_other.rect.right - sprite.rect.left)
 
-    if sprite.rect.centery < sprite_other.rect.centery:
-        overlap_y = sprite.rect.bottom - sprite_other.rect.top
-    elif sprite.rect.centery > sprite_other.rect.centery:
-        overlap_y = -(sprite_other.rect.bottom - sprite.rect.top)
+        if sprite.rect.centery < sprite_other.rect.centery:
+            overlap_y = sprite.rect.bottom - sprite_other.rect.top
+        elif sprite.rect.centery > sprite_other.rect.centery:
+            overlap_y = -(sprite_other.rect.bottom - sprite.rect.top)
 
     return overlap_x, overlap_y
 
